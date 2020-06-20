d34d066e20b832fcf742944531b6a2a1609549f7
Denys <herasymuk@ucu.edu.ua>
2020-06-20 18:41:23 +0300

    commit after passing task

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask_app.RSA import decoder
+from flask_app.RSA import decoder, encoder
 
 app = Flask(__name__)
 
@@ -8,21 +8,44 @@ app = Flask(__name__)
 def input_profession():
     """
     Function for generating main web-page
-    :return: rendered html template
+    :return: rendered html template with decoded or encoded message
     """
-    response = ""
+    response_decoded = ""
+    response_encoded = ""
+
     if request.method == 'POST':
         response = "Invalid input"
-        message = request.form['message']
-        try:
-            p = int(request.form['p'])
-            q = int(request.form['q'])
-            e = int(request.form['e'])
-            if p * q < 255255:
-                raise ValueError
-        except ValueError:
-            return render_template("index.html", decoded=response)
-        if message and p and q and e:
-            response = decoder(message, p, q, e)
-    return render_template("index.html", decoded=response)
+        simple_message = request.form['message0']
+        encoded_message = request.form['message']
 
+        if simple_message:
+            try:
+                p0 = int(request.form['p0'])
+                q0 = int(request.form['q0'])
+                e0 = int(request.form['e0'])
+                if p0 * q0 < 255255:
+                    raise ValueError
+            except ValueError:
+                return render_template("index.html", decoded=response, encoded=response)
+
+        if encoded_message:
+            try:
+                p = int(request.form['p'])
+                q = int(request.form['q'])
+                e = int(request.form['e'])
+                if p * q < 255255:
+                    raise ValueError
+            except ValueError:
+                return render_template("index.html", decoded=response, encoded=response)
+
+        if simple_message and p0 and q0 and e0:
+            response_encoded = encoder(simple_message, p0, q0, e0)
+
+        if encoded_message and p and q and e:
+            response_decoded = decoder(encoded_message, p, q, e)
+
+    return render_template("index.html", decoded=response_decoded, encoded=response_encoded)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
